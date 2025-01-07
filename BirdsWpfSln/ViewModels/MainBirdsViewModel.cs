863a6cbe64840a73d9a4c632d4369e5ed6c49c99
EldHasp <eldhasp@gmail.com>
2025-01-08 01:39:35 +0300

    В интерфейс Навигатора добавлена реализация членов. В ViewModels и Views внесены изменения для использования этой реализации.

--- BirdsWpfSln/ViewModels/MainBirdsViewModel.cs
+++ BirdsWpfSln/ViewModels/MainBirdsViewModel.cs
@@ -12,54 +12,42 @@ namespace BirdsViewModels
         private readonly AddBirdViewModel addBirdVM;
         private static readonly ReadOnlyCollection<string> privateBirdNameGroups
             = Array.AsReadOnly(["Nuthatch", "Great tit", "Black-capped chickadee", "Sparrow", "Amadina", "All of them", "Only inactive", "Only active"]);
+        private readonly INavigationService navigator;
         #endregion
 
         public MainBirdsViewModel(IRepository<Bird> birdsRepository)
         {
             this.birdsRepository = birdsRepository;
             birdsVM = new(birdsRepository);
-            Current = addBirdVM = new(birdsRepository);
             addBirdVM = new(birdsRepository);
 
             Birds = birdsRepository.GetObservableCollection();
+
+            navigator = this;
+            navigator.NavigateTo(addBirdVM = new(birdsRepository));
+            navigator.AddCreator(typeof(BirdsViewModel), () => this.birdsVM);
+            navigator.AddCreator(typeof(AddBirdViewModel), () => this.addBirdVM);
         }
+
         /// <summary>Предоставляет статическую коллекцию <see cref="privateBirdNameGroups"/>. 
         /// Можно было обойтись статическим полем, но для облегчения привязок создано это прокси свойство.</summary>
         public ReadOnlyCollection<string> BirdNameGroups => privateBirdNameGroups;
 
         #region Properties
         public ReadOnlyObservableCollection<Bird> Birds { get; }
-        public object? Current { get => Get<object>(); private set => Set(value); }
+
+
         public DateOnly Departure { get; } = DateOnly.FromDateTime(DateTime.Now);
         #endregion
 
         #region Methods
-        public void NavigateTo(object? viewModel)
-        {
-            Current = viewModel;
-        }
         public async Task LoadAsync() => await birdsRepository.LoadAsync();
+
+        public void RaiseCurrentChanged() => RaisePropertyChanged(nameof(INavigationService.Current));
+
         #endregion
 
         #region Commands
-        public RelayCommand NavigateToType => GetCommand<Type>(t =>
-        {
-            object? curr;
-            if (t == typeof(BirdsViewModel))
-            {
-                curr = birdsVM;
-            }
-            else if (t == typeof(AddBirdViewModel))
-            {
-                curr = addBirdVM;
-            }
-            else
-            {
-                curr = null;
-            }
-            NavigateTo(curr);
-        });
-
         public RelayCommand AddBirdCommand => GetCommand<AddBirdViewModel>
         (
             async birdVM =>
@@ -69,13 +57,13 @@ namespace BirdsViewModels
             },
             birdVM => !string.IsNullOrWhiteSpace(birdVM.Name)
         );
+
         public RelayCommand DeleteBirdCommand => GetCommand<Bird>
         (
             async bird =>
             {
-
                 // Создание клона с внесёнными изменениями, которые отобразятся только после сохранения в Репозитории.
-                Bird bird1 = new (bird.Id, bird.Name, bird.Description, bird.Arrival, bird.Departure, false);
+                Bird bird1 = new(bird.Id, bird.Name, bird.Description, bird.Arrival, bird.Departure, false);
 
                 await birdsRepository.UpdateAsync(bird1);
             },
