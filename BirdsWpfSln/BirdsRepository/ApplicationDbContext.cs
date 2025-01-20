944587928b1ae8cb3054a0c46346200132101504
Londongrad <h-b-12@mail.ru>
2025-01-20 23:41:00 +0900

    Revert "Попытка инкапсуляции параметров конфигурации для DbContext."
    This reverts commit ed4f18a98af024efa015c25814b3c1025a8c4ad2.

--- BirdsWpfSln/BirdsRepository/ApplicationDbContext.cs
+++ BirdsWpfSln/BirdsRepository/ApplicationDbContext.cs
@@ -5,15 +5,29 @@ using System.Windows.Media;
 
 namespace BirdsRepository
 {
-    public class ApplicationDbContext(string dbFullName, DbContextOptions<ApplicationDbContext> options) : DbContext(options)
+    internal class ApplicationDbContext(string dbFullName) : DbContext
     {
         public DbSet<Bird>? Birds { get; set; }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.ApplyConfiguration(new BirdConfiguration());
 
             base.OnModelCreating(modelBuilder);
         }
+
         public string DbFullName { get; } = dbFullName;
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            //_ = optionsBuilder.UseSqlite(DbFullName);
+
+            //optionsBuilder
+            //    .EnableSensitiveDataLogging()
+            //    .UseSqlite($"Data Source={DbFullName}");
+
+            //optionsBuilder.LogTo(Console.WriteLine, [RelationalEventId.CommandExecuted]);
+            optionsBuilder.UseSqlServer("Data Source=DESKTOP-9OKU3FE\\SQLEXPRESS;Initial Catalog=Birds;Integrated Security=True;Encrypt=False;Trust Server Certificate=True");
+        }
     }
 }
--- BirdsWpfSln/BirdsWPF/MainBirdsViewModelExtension.cs
+++ BirdsWpfSln/BirdsWPF/MainBirdsViewModelExtension.cs
@@ -1,7 +1,5 @@
 ﻿using BirdsRepository;
 using BirdsViewModels;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 using System.Windows.Markup;
 
 namespace BirdsWPF
@@ -21,18 +19,7 @@ namespace BirdsWPF
 
         public override object ProvideValue(IServiceProvider serviceProvider)
         {
-            var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-            var options = optionsBuilder.UseSqlite(DbFileName).Options;
-            return new MainBirdsViewModel(new DbBirdsRepository(DbFileName!, options));
-            //_ = optionsBuilder.UseSqlite(DbFullName);
-
-            //optionsBuilder
-            //    .EnableSensitiveDataLogging()
-            //    .UseSqlite($"Data Source={DbFullName}");
-
-            //optionsBuilder.LogTo(Console.WriteLine, [RelationalEventId.CommandExecuted]);
-
-            //optionsBuilder.UseSqlServer("Data Source=DESKTOP-9OKU3FE\\SQLEXPRESS;Initial Catalog=Birds;Integrated Security=True;Encrypt=False;Trust Server Certificate=True");
+            return new MainBirdsViewModel(new DbBirdsRepository(DbFileName!));
         }
     }
 }
